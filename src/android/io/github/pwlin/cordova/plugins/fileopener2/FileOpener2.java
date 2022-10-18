a4dd20bd137db8d4d79d4caaa2318684b9e129e1
vladretca <vlad.retca@gmail.com>
2022-10-18 22:17:16 +0300

    Update FileOpener2.java
    bugfix download android package

--- src/android/io/github/pwlin/cordova/plugins/fileopener2/FileOpener2.java
+++ src/android/io/github/pwlin/cordova/plugins/fileopener2/FileOpener2.java
@@ -108,27 +108,11 @@ public class FileOpener2 extends CordovaPlugin {
 				}
 
 				Intent intent;
-				if (contentType.equals("application/vnd.android.package-archive")) {
-					// https://stackoverflow.com/questions/9637629/can-we-install-an-apk-from-a-contentprovider/9672282#9672282
-					intent = new Intent(Intent.ACTION_INSTALL_PACKAGE);
-					Uri path;
-					if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
-						path = Uri.fromFile(file);
-					} else {
-						Context context = cordova.getActivity().getApplicationContext();
-						path = FileProvider.getUriForFile(context, cordova.getActivity().getPackageName() + ".fileOpener2.provider", file);
-					}
-					intent.setDataAndType(path, contentType);
-					intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_ACTIVITY_NEW_TASK);
-
-				} else {
-					intent = new Intent(Intent.ACTION_VIEW);
-					Context context = cordova.getActivity().getApplicationContext();
-					Uri path = FileProvider.getUriForFile(context, cordova.getActivity().getPackageName() + ".fileOpener2.provider", file);
-					intent.setDataAndType(path, contentType);
-					intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-
-				}
+				intent = new Intent(Intent.ACTION_VIEW);
+				Context context = cordova.getActivity().getApplicationContext();
+				Uri path = FileProvider.getUriForFile(context, cordova.getActivity().getPackageName() + ".fileOpener2.provider", file);
+				intent.setDataAndType(path, contentType);
+				intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
 
 				/*
 				 * @see
